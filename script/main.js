da44a785cfb9a5430286879f0a03a31914a99610
hun <118506932+msheckles@users.noreply.github.com>
2024-06-02 12:22:05 +0330

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -3,14 +3,16 @@ let Single = document.getElementById("Single");
 let Multiple = document.getElementById("Multiple");
 let Back = document.getElementById("Back");
 
+let multiple = false;
+
 Single.style.display = "none";
 Multiple.style.display = "none";
 Back.style.display = "none";
 
-var pScore = 0;
-var aiScore = 0;
-var player1Score = 0;
-var player2Score = 0;
+let pScore = 0;
+let aiScore = 0;
+let player1Score = 0;
+let player2Score = 0;
 
 let a = document.getElementById("a");
 let pScore1 = document.getElementById("pScore");
@@ -20,19 +22,24 @@ let comp = document.getElementById("comp");
 let b = document.getElementById("b");
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
-let check1 = document.getElementById("check1");
+let check11 = document.getElementById("check1");
 
 let p1 = null;
 let p2 = null;
 
 
-function menu() {
+function reset() {
     Intro.style.display = "block";
     Single.style.display = "none";
     Multiple.style.display = "none";
     Back.style.display = "none";
+    multiple = false;
     pScore = 0;
     aiScore = 0;
+    player1Score = 0;
+    player1.innerHTML = "0";
+    player2Score = 0;
+    player2.innerHTML = "0";
 }
 
 
@@ -41,73 +48,59 @@ function check1() {
     Intro.style.display = "none";
     Single.style.display = "block";
     Back.style.display = "block";
-    Single.style.display = "block";
-    one.innerHTML = "Rock Paper Scissors";
-    one.style.color = "black";
-    two.innerHTML = "0";
-    three.innerHTML = "0";
+    a.innerHTML = "Rock Paper Scissors";
+    a.style.color = "black";
+    pScore1.innerHTML = "0";
+    aiScore1.innerHTML = "0";
 }
 
 function check2() {
     Intro.style.display = "none";
     Multiple.style.display = "block";
     Back.style.display = "block";
-    Multiple.style.display = "block";
-    one1.innerHTML = "Rock Paper Scissors";
+    multiple = true;
+    b.innerHTML = "Rock Paper Scissors";
     player1.innerHTML = "0"
     player1.innerHTML = "0";
-    check1.innerHTML = "START";
+    check11.innerHTML = "START";
 }
 
 function checking() {
     let bSingle = document.getElementById("bSingle");
     bSingle.innerHTML = "Play Again!";
     let player = document.querySelector('input[name="run"]:checked').value;
-    console.log(player);
     const choises = ["Paper", "Scissors", "Rock"];
     let ai = choises[Math.floor(Math.random() * choises.length)];
-    console.log(ai);
 
     if (player == ai) {
-        r1(0, ai);
+        a.innerHTML = "DRAW!";
+        a.style.color = "white";
     } else if (
         (player == "Paper" && ai == "Scissors") ||
         (player == "Scissors" && ai == "Rock") ||
         (player == "Rock" && ai == "Paper")) {
         aiScore++;
-        r1(-1, ai);
+        a.innerHTML = "You Lost!";
+        a.style.color = "red";
     } else {
         pScore++;
-        r1(1, ai);
+        a.innerHTML = "You Won!";
+        a.style.color = "green";
     }
-}
 
-function r1(s, u) {
-    if (s == -1) {
-        one.innerHTML = "You Lost!";
-        one.style.color = "red";
-    } else if (s == 1) {
-        one.innerHTML = "You Won!";
-        one.style.color = "green";
-    } else {
-        one.innerHTML = "DRAW!";
-        one.style.color = "white";
-    }
-    two.innerHTML = pScore;
-    three.innerHTML = aiScore;
-    comp.innerHTML = u;
+    pScore1.innerHTML = pScore;
+    aiScore1.innerHTML = aiScore;
+    comp.innerHTML = ai;
 }
 
 document.onkeydown = function(event) {
     if (multiple) {
-        if (check1 == "START") {
+        if (check11.innerHTML == "START") {
             keyPressedChecker(event);
         } else if (event.key == " ") {
-            headerBoardMulti.innerHTML = "Rock Paper Scissors";
-            headerBoardMulti.style.color = "white";
-            turnBoardMulti.innerHTML = "START";
-            player1Select.innerHTML = '';
-            player2Select.innerHTML = '';
+            b.innerHTML = "Rock Paper Scissors";
+            b.style.color = "white";
+            check11.innerHTML = "START";
         }
     }
 }
@@ -131,40 +124,44 @@ function keyPressedChecker(event) {
         p2 = "Rock";
     }
 
-    checking1(p1, p2);
+    if ((p1 != null) && (p2 != null)) {
+        multipleCheck(p1, p2);
+        p1 = null;
+        p2 = null;
+    }
 }
 
-function checking1(playerOne, playerTwo) {
-    if (playerOne == playerTwo) {
+function multipleCheck(one, two) {
+    if (one == two) {
         b.innerHTML = "DRAW";
         b.style.color = "white";
-    } else if (playerOne == "Rock" && playerTwo == "Paper") {
-        b.innerHTML = "Player 2 Wins!";
-        b.style.color = custom_red;
-        player2Score++;
-    } else if (playerOne == "Rock" && playerTwo == "Scissors") {
-        b.innerHTML = "Player 1 Wins!";
-        b.style.color = "green";
-        player1Score++;
-    } else if (playerOne == "Paper" && playerTwo == "Rock") {
-        b.innerHTML = "Player 1 Wins!";
-        b.style.color = "green";
-        player1Score++;
-    } else if (playerOne == "Paper" && playerTwo == "Scissors") {
+    } else if (one == "Rock" && two == "Paper") {
         b.innerHTML = "Player 2 Wins!";
         b.style.color = "red";
         player2Score++;
-    } else if (playerOne == "Scissors" && playerTwo == "Paper") {
+    } else if (one == "Rock" && two == "Scissors") {
+        b.innerHTML = "Player 1 Wins!";
+        b.style.color = "green";
+        player1Score++;
+    } else if (one == "Paper" && two == "Rock") {
+        b.innerHTML = "Player 1 Wins!";
+        b.style.color = "green";
+        player1Score++;
+    } else if (one == "Paper" && two == "Scissors") {
+        b.innerHTML = "Player 2 Wins!";
+        b.style.color = "red";
+        player2Score++;
+    } else if (one == "Scissors" && two == "Paper") {
         b.innerHTML = "Player 1 Wins!";
         b.style.color = "red";
         player1Score++;
-    } else if (playerOne == "Scissors" && playerTwo == "Rock") {
-        headerBoardMulti.innerHTML = "Player 2 Wins!";
-        headerBoardMulti.style.color = custom_red;
+    } else if (one == "Scissors" && two == "Rock") {
+        b.innerHTML = "Player 2 Wins!";
+        b.style.color = "red";
         player2Score++;
     }
 
-    check1.innerHTML = "PRESS \"ENTER\" KEY TO PLAY AGAIN";
-    p1 = null;
-    p2 = null;
+    check11.innerHTML = "PRESS \"SPACE\" KEY TO PLAY AGAIN";
+    player1.innerHTML = player1Score;
+    player2.innerHTML = player2Score;
 }
